9a35104401d2fcc79d0d5c0ba01e5b67e0ddff50
coltkain <kain.coltx@gmail.com>
2024-02-03 19:01:42 +0200

    feat: create the header for file operations

---
+++ include/file_operations.h
@@ -0,0 +1,8 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+void create_file(const char* file_name);
+void rename_file(const char* file_name);
+void delete_file(const char* file_name);
+
+#endif
