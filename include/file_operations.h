692438fbe82cd0a4cd69f1060a87d99dd7a82ec3
coltkain <kain.coltx@gmail.com>
2024-02-03 19:19:36 +0200

    fix: add an additional parameter for a file operation

--- include/file_operations.h
+++ include/file_operations.h
@@ -2,7 +2,7 @@
 #define FILE_OPERATIONS_H
 
 void create_file(const char* file_name);
-void rename_file(const char* file_name);
+void rename_file(const char* file_name, const char* new_file_name);
 void delete_file(const char* file_name);
 
 #endif
